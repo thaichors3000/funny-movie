296c325cab5297a805f4eea4d4f409210cd37c23
thaichor <thaichor@gmail.com>
2020-07-13 03:13:33 +0700

    add validation to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, length: { maximum: 100 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:username).is_at_most(100) }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(8) }
+  end
 end
