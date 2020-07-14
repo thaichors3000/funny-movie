31d301a5e8649db68f2e2be39766bc23c159746d
thaichor <thaichor@gmail.com>
2020-07-14 16:39:39 +0700

    register new user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_initialize_by(username: param_username)
+
+    if @user.new_record?
+      @user.password = param_password
+      @user.save!
+    end
+
+    session[:user_id] = @user.id
+
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def param_username
+    params.require(:username)
+  end
+
+  def param_password
+    params.require(:password)
+  end
+end
